1c60b34b30c6589c3cbc3f65262605ed2461e6f5
YannPrtm <112480275+YannPrtm@users.noreply.github.com>
2022-09-18 13:14:54 +0800

    Add files via upload

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['6282252285143'] //ur owner number
+global.ownername = "Ryjrkanism" //ur owner name
+global.ytname = "YT: " //ur yt chanel name
+global.socialm = "GitHub: Ryjrka" //ur github or insta name
+global.location = "Spanyol, Barcelona" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['6282252285143']
+global.ownertag = '6282252285143' //ur tag number
+global.botname = '𝐑𝐲𝐣𝐫𝐤𝐚-𝐁𝐨𝐭' //ur bot name
+global.linkz = "-" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtu.be/" //ur website to be displayed
+global.botscript = 'https://wa.me/6282252285143' //script link
+global.reactmoji = "💲" //ur menu react emoji
+global.themeemoji = "💱" //ur theme emoji
+global.packname = "𝖱𝖸𝖩𝖱𝖪𝖠 𝖡𝖮𝖳 ʙʏ\n\n\n\n\n\n\n\n\n\n\n𝐑𝐲𝐣𝐫𝐤𝐚𝐍𝐢𝐬𝐦 " //ur sticker watermark packname
+global.author = "Ryjrkanism" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['6282252285143'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
